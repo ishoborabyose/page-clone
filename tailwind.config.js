ed508ed995e0fb6e60d600c649db3eac68072daf
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-09-27 12:42:15 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ module.exports = {
     './index.html',
   ],
   theme: {
-    extend: {},
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      extend: {
+        colors: {
+          brightRed: '#F53838',
+          brightWhiteBackground: '#F6F6F6',
+          darkBlue: '#0B132A',
+          darkGrayishBlue: '#4F5665',
+          borderColors: '#DDDDDD',
+          titleColor: '#0B132A',
+        },
+  
+        dropShadow: {
+          '3xl': '0 35px 35px #F53838',
+          '4xl': ['0 35px 35px #F53838', '0 45px 65px #F53836'],
+        },
+      },
+    },
   },
   plugins: [],
 }
